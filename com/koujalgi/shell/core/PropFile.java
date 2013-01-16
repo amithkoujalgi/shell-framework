7f614b92980e82e1c9619a37882c942dbc06fd48
Amith Koujalgi <koujalgi.amith@gmail.com>
2013-01-16 18:20:31 +0530

    added properties file util

---
+++ com/koujalgi/shell/core/PropFile.java
@@ -0,0 +1,28 @@
+package com.koujalgi.shell.core;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropFile {
+	private String propFile;
+
+	public PropFile(String propFile) {
+		this.propFile = propFile;
+	}
+
+	public String getProperty(String key) throws FileNotFoundException,
+			IOException {
+		Properties prop = new Properties();
+		prop.load(new FileInputStream(propFile));
+		return prop.getProperty(key);
+	}
+
+	public void setProperty(String key, String value)
+			throws FileNotFoundException, IOException {
+		Properties prop = new Properties();
+		prop.setProperty(key, value);
+		prop.store(new FileOutputStream(propFile), null);
+	}
+}
